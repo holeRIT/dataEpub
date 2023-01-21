b10b12f176bc83f195af976ddc5582d4ef9ae78f
holeRIT <118928065+holeRIT@users.noreply.github.com>
2023-01-21 15:27:06 +0100

    Add files via upload

---
+++ deplacement.js
@@ -0,0 +1,4 @@
+document.perso.definitTrajectoire([322,99,835,1014],[207,717,41,764]);
+document.perso.deplaceTg(105,1500);
+//document.perso.deplaceTgPi(105,1500);
+//document.perso.deplaceNonTg(105,1500);
